154c57b5b325333b03e7e4e3280ac9a238ca0ac2
Liam Floyd <liamf1764@gmail.com>
2023-07-06 19:18:52 -0500

    Add UPNP setup & export config

--- world.gd
+++ world.gd
@@ -27,13 +27,15 @@ func _on_host_button_pressed():
 	
 	# Spawn host player
 	add_player(multiplayer.get_unique_id())
+	
+	upnp_setup()
 
 
 func _on_join_button_pressed():
 	mainMenu.hide()
 	hud.show()
 	
-	enetPeer.create_client("localhost", PORT)	
+	enetPeer.create_client(addressEntry.text, PORT)	
 	multiplayer.multiplayer_peer = enetPeer
 
 func add_player(peer_id):
@@ -55,3 +57,16 @@ func update_health_bar(health_value):
 func _on_multiplayer_spawner_spawned(player):
 	if player.is_multiplayer_authority():
 		player.health_changed.connect(update_health_bar)
+
+func upnp_setup():
+	var upnp = UPNP.new()
+	
+	var discover_result = upnp.discover()
+	assert(discover_result == UPNP.UPNP_RESULT_SUCCESS, "UPNP Discover Failed! Error %s" % discover_result)
+	
+	assert(upnp.get_gateway() and upnp.get_gateway().is_valid_gateway(), "UPNP Invalid Gateway")
+	
+	var map_result = upnp.add_port_mapping(PORT)
+	assert(map_result == UPNP.UPNP_RESULT_SUCCESS, "UPNP Port Mapping Failed! Error %s" % map_result)
+	
+	print("Success! Join Address: %s" % upnp.query_external_address()) 
